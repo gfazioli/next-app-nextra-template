167f3540c8ac2e2c26fdf1ab34b41a861d4b8083
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-03-29 17:20:51 +0200

    refactor(Content): Remove sponsor card 🗑️
    - Drop Flex import and sponsor iframe from Content component
    - Simplify layout and reduce unused code
    - Decrease bundle size and improve load performance

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -1,5 +1,5 @@
 import { Marquee } from '@gfazioli/mantine-marquee';
-import { Button, Flex, Stack, Text } from '@mantine/core';
+import { Button, Stack, Text } from '@mantine/core';
 
 export const Content = () => {
   return (
@@ -72,19 +72,6 @@ export const Content = () => {
         </Marquee>
       </Stack>
 
-      <Flex align="center" justify="center" my="2vh">
-        <iframe
-          src="https://github.com/sponsors/gfazioli/card"
-          title="Sponsor gfazioli"
-          height="225"
-          width="100%"
-          style={{
-            border: '1px solid rgb(255,0,0,0.2)',
-            borderRadius: 8,
-            boxShadow: '0 1px 2px rgba(0, 0, 0, 0.3)',
-          }}
-        />
-      </Flex>
     </>
   );
 };
