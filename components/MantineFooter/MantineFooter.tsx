c94d7960db2d2555906aa8a10cb588df5189647c
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-03-29 17:25:22 +0200

    feat(MantineFooter): add footer component with animated badge and link sections 🚀
    - Adds a fully styled MantineFooter with social icons, sponsor button, and copyright notice.
    - Introduces AnimateBadge component with pulse animation for new items.
    - Provides organized link lists for highlights, resources, and ecosystem.
    - Utilizes Mantine, Nextra, and Tabler icons for a cohesive UI.

--- components/MantineFooter/MantineFooter.tsx
+++ components/MantineFooter/MantineFooter.tsx
@@ -1,17 +1,170 @@
-import { Footer } from 'nextra-theme-docs';
-import { Anchor, Box } from '@mantine/core';
+'use client';
 
-/**
- * You can customize the Nextra Footer component.
- * Don't forget to use the MantineProvider component.
- *
- * @since 1.0.0
- *
- */
-export const MantineFooter = () => (
-  <Box style={{ position: 'relative' }}>
-    <Footer>
-      MIT {new Date().getFullYear()} © <Anchor href="https://undolog.com">Undolog</Anchor>
-    </Footer>
-  </Box>
-);
+import {
+  IconBrandGithubFilled,
+  IconBrandMantine,
+  IconBrandX,
+  IconMailHeart,
+} from '@tabler/icons-react';
+import {
+  ActionIcon,
+  Anchor,
+  Container,
+  Divider,
+  Grid,
+  Group,
+  Stack,
+  Text,
+  ThemeIcon,
+  Title,
+} from '@mantine/core';
+import { Logo } from '@/components/Logo/Logo';
+import packageJson from '../../package.json';
+import { AnimateBadge } from './AnimateBadge';
+import { ecosystem, highlights, resources } from './links';
+import classes from './MantineFooter.module.css';
+
+type VerticalLink = {
+  key: string;
+  title: string;
+  href: string;
+  newWindow?: boolean;
+  new?: boolean;
+};
+
+const VerticalLinks = ({ list }: { list: VerticalLink[] }) => {
+  return (
+    <>
+      {list.map((item) => (
+        <Group key={item.key}>
+          <Anchor
+            className={classes.columnAnchor}
+            href={item.href}
+            target={item.newWindow ? '_blank' : undefined}
+            rel={item.newWindow ? 'noopener noreferrer' : undefined}
+          >
+            {item.title}
+          </Anchor>
+          {item.new && <AnimateBadge />}
+        </Group>
+      ))}
+    </>
+  );
+};
+
+export const MantineFooter = () => {
+  return (
+    <div className={classes.contentFooter}>
+      <Container className={classes.footer} size="lg">
+        <Grid grow>
+          <Grid.Col span={{ base: 12, sm: 4 }}>
+            <Stack gap="xs">
+              <ThemeIcon>
+                <Logo />
+              </ThemeIcon>
+              <Text fz={13} mr={64}>
+                This is a template for Next.js + Mantine + Nextra documentation sites. Feel free to
+                use it and{' '}
+                <Anchor fz={13} href={`https://github.com/gfazioli/next-app-nextra-template`}>
+                  contribute to it
+                </Anchor>
+                . Don't forget to star it on{' '}
+                <Anchor fz={13} href={`https://github.com/gfazioli/next-app-nextra-template`}>
+                  GitHub
+                </Anchor>
+                . And if you wish, you can also follow me on{' '}
+                <Anchor fz={13} href="https://twitter.com/gfazioli">
+                  Twitter
+                </Anchor>
+                . Obviously, you can also{' '}
+                <Anchor fz={13} href="https://github.com/sponsors/gfazioli">
+                  donate
+                </Anchor>{' '}
+                to support the development of this project.
+              </Text>
+              <Group>
+                <ActionIcon variant="subtle" component="a" href="https://github.com/gfazioli">
+                  <IconBrandGithubFilled size={24} />
+                </ActionIcon>
+                <ActionIcon variant="subtle" component="a" href="https://twitter.com/gfazioli">
+                  <IconBrandX size={24} />
+                </ActionIcon>
+                <ActionIcon variant="subtle" component="a" href="https://undolog.substack.com/">
+                  <IconMailHeart size={24} />
+                </ActionIcon>
+              </Group>
+            </Stack>
+          </Grid.Col>
+          <Grid.Col className={classes.column} span={2}>
+            <Stack gap="xs">
+              <Title className={classes.title} order={6}>
+                HIGHLIGHTS
+              </Title>
+              <VerticalLinks list={highlights} />
+            </Stack>
+          </Grid.Col>
+          <Grid.Col className={classes.column} span={2}>
+            <Stack gap="xs">
+              <Title className={classes.title} order={6}>
+                RESOURCES
+              </Title>
+              <VerticalLinks list={resources} />
+            </Stack>
+          </Grid.Col>
+          <Grid.Col className={classes.column} span={2}>
+            <Stack gap="xs">
+              <Title className={classes.title} order={6}>
+                ECOSYSTEM
+              </Title>
+              <VerticalLinks list={ecosystem} />
+            </Stack>
+          </Grid.Col>
+        </Grid>
+
+        <Divider my={16} className={classes.lastDivider} />
+
+        <Group justify="space-between">
+          <iframe
+            src="https://github.com/sponsors/gfazioli/button"
+            title="Sponsor gfazioli"
+            height="32"
+            width="114"
+            style={{ border: 0 }}
+            sandbox="allow-popups allow-forms allow-scripts"
+          />
+
+          <Group justify="right">
+            <Text fz={12} inline>
+              Made with ❤️ by{' '}
+              <Anchor fz={13} href="https://undolog.com/">
+                Undolog
+              </Anchor>
+            </Text>
+            <Divider orientation="vertical" />
+            <Text fz={12} inline>
+              <Group gap={4} component="span">
+                Hosted on{' '}
+                <Anchor fz={13} href="https://github.com/">
+                  <Group gap={4} component="span">
+                    <IconBrandGithubFilled size={16} /> GitHub.com
+                  </Group>
+                </Anchor>
+              </Group>
+            </Text>
+            <Divider orientation="vertical" />
+            <Text fz={12} inline>
+              <Group gap={4} component="span" justify="flex-start">
+                Built with{' '}
+                <Anchor fz={13} href="https://mantine.dev/">
+                  <Group gap={4} component="span">
+                    <IconBrandMantine size={16} /> Mantine v{packageJson.dependencies['@mantine/core']}
+                  </Group>
+                </Anchor>
+              </Group>
+            </Text>
+          </Group>
+        </Group>
+      </Container>
+    </div>
+  );
+};
