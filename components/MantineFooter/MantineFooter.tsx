d07f0e842f124b18f7ab94e18c533c4ac0656d88
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-06-04 13:52:35 +0200

    fix(footer): Undolog newsletter link → undolog.com

--- components/MantineFooter/MantineFooter.tsx
+++ components/MantineFooter/MantineFooter.tsx
@@ -107,7 +107,7 @@ export const MantineFooter = () => {
                 <ActionIcon variant="subtle" component="a" href="https://twitter.com/gfazioli">
                   <IconBrandX size={24} />
                 </ActionIcon>
-                <ActionIcon variant="subtle" component="a" href="https://undolog.substack.com/">
+                <ActionIcon variant="subtle" component="a" href="https://undolog.com/">
                   <IconMailHeart size={24} />
                 </ActionIcon>
               </Group>
